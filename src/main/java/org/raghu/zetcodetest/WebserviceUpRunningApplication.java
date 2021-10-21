f68f7dfb1094a6d08053ec1edc62159c13d2c8cf
Raghavendra GR <raghavendragr@Raghavendras-MacBook-Pro.local>
2021-10-21 08:27:41 +0530

    Initial Commit

---
+++ src/main/java/org/raghu/zetcodetest/WebserviceUpRunningApplication.java
@@ -0,0 +1,13 @@
+package org.raghu.zetcodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceUpRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceUpRunningApplication.class, args);
+	}
+
+}
